364dd1b9d01b6d97d125ef6d90527f0f3e28bf97
Niklas Edmundsson <nikke@hpc2n.umu.se>
2020-05-12 20:41:54 +0200

    Support regex as RRD names

--- set-ganglia-rrd-limits.pl
+++ set-ganglia-rrd-limits.pl
@@ -44,6 +44,7 @@ my $help;
 
 # Lovely global variables :-)
 my %tunings;
+my %relist;
 my @rrdtuneargs;
 
 my %tune2info = (
@@ -51,14 +52,24 @@ my %tune2info = (
 	'--minimum' => 'min',
 );
 
+my %flaghelp = (
+	suminfoprod => 'SummaryInfo RRD limits are multiplied by num (default)',
+	suminfosqrt => 'SummaryInfo RRD limits are multiplied by sqrt(num)',
+	suminfoval  => 'SummaryInfo RRD limits are set to the given value',
+	regex => 'RRD name is a regex',
+# FIXME: Future stuff
+	#sumnodec => 'Don't decrease values already set in SummaryInfo RRDs',
+	#dir => 'RRD name includes directory component',
+);
+
 
 # ------------------------------------------------------------
 
 sub usage
 {
-	print STDERR <<EOH
+	print STDERR <<EOH;
 
-Usage: $0 name.rrd:--[minimum|maximum]|value ...
+Usage: $0 name.rrd:--[minimum|maximum]:value[:flag1,..,flagN] ...
 
 Options:
 	--quiet   - Be quiet
@@ -72,6 +83,12 @@ Environment variables:
 	RRDCACHED_ADDRESS - rrdcached address
 	
 EOH
+
+	print STDERR "Per RRD flags understood:\n";
+	foreach my $f (sort keys %flaghelp) {
+		print STDERR "    $f: $flaghelp{$f}\n";
+	}
+
 }
 
 sub message
@@ -97,54 +114,90 @@ sub debug
 
 sub handle_rrd
 {
-	my ($rrd, $file, $multiplier) = @_;
+    my ($matches, $file) = @_;
 
-	my $info = RRDs::info $file;
-	my $rc = RRDs::error;
+    my $info = RRDs::info $file;
+    my $rc = RRDs::error;
 
-	die "info error on $file: $rc" if($rc);
+    die "info error on $file: $rc" if($rc);
 
-	debug "$file info:\n", Dumper $info;
+    debug "$file info:\n", Dumper $info;
 
-	foreach my $k (sort keys %{$tunings{$rrd}}) {
-		my $v = $tunings{$rrd}{$k};
-		if($multiplier) {
-			$v *= $multiplier;
-		}
-		# Hard-coded, each Ganglia RRD only have the
-		# sum data store.
-		my $is = "ds[sum].$tune2info{$k}";
-		my $oldval = "N/A";
-		my $newval = format_bytes($v, bs=>1000);
-		if(defined($info->{$is})) {
-			$oldval = format_bytes($info->{$is}, bs=>1000);
-		}
+    debug "matches:\n", Dumper $matches;
 
-		if(defined($info->{$is}) && $info->{$is} == $v) {
-			verbose "$file: $k already $oldval, skipping\n";
-			next;
-		}
+    foreach my $match (@{$matches}) {
+        foreach my $k (sort keys %{$match}) {
+            my $v = $match->{$k}{value};
+            if($info->{'ds[num].last_ds'}) {
+                # If it has a num DS it's a SummaryInfo RRD
+                if($match->{$k}{flags}{suminfosqrt}) {
+                    $v *= int(sqrt(abs($info->{'ds[num].last_ds'})));
+                    if($info->{'ds[num].last_ds'} < 0) {
+                        $v *= -1;
+                    }
+                    debug "Value modified by suminfosqrt flag\n";
+                }
+                elsif($match->{$k}{flags}{suminfoval}) {
+                    debug "Value unmodified due to suminfoval flag\n";
+                }
+                else {
+                    $v *= $info->{'ds[num].last_ds'};
+                    debug "Value modified by suminfoprod flag\n";
+                }
+            }
+            # Hard-coded, each Ganglia RRD only have the
+            # sum data store.
+            my $is = "ds[sum].$tune2info{$k}";
+            my $oldval = "N/A";
+            my $newval = format_bytes($v, bs=>1000);
+            if(defined($info->{$is})) {
+                $oldval = format_bytes($info->{$is}, bs=>1000);
+            }
 
-		if($dryrun) {
-			message "$file: want to tune $k from $oldval to $newval\n";
-		}
-		else {
-			message "$file: tuning $k from $oldval to $newval\n";
-			RRDs::tune $file, @rrdtuneargs, $k, "sum:$v";
-			$rc = RRDs::error;
-			die "Error tuning $k to $v on $file: $rc" if($rc);
-		}
-	}
+            if($oldval eq $newval) {
+                verbose "$file: $k already $oldval, skipping\n";
+                next;
+            }
+
+            if($dryrun) {
+                message "$file: want to tune $k from $oldval to $newval\n";
+            }
+            else {
+                message "$file: tuning $k from $oldval to $newval\n";
+                RRDs::tune $file, @rrdtuneargs, $k, "sum:$v";
+                $rc = RRDs::error;
+                die "Error tuning $k to $v on $file: $rc" if($rc);
+            }
+        }
+    }
 			
 }
 
 sub handle_dir
 {
-	my ($dir, $multiplier) = @_;
+	my ($dir) = @_;
 
 	my $handled = 0;
 
-	foreach my $rrd (sort keys %tunings) {
+	opendir(my $dh, $dir) || die "opendir $dir: $!";
+	my @rrds = sort grep { $_ =~ /.rrd$/ } readdir($dh);
+	closedir($dh);
+
+	foreach my $rrd (sort @rrds) {
+		my $matches;
+		if($tunings{$rrd}) {
+			push @{$matches}, $tunings{$rrd};
+		}
+		if(!$matches) {
+			foreach my $re (keys %relist) {
+				if($rrd =~ m !$re!) {
+					push @{$matches}, $relist{$re};
+				}
+			}
+		}
+
+		next unless($matches);
+
 		my $f = "$dir/$rrd";
 
 		next unless(-f $f);
@@ -158,7 +211,7 @@ sub handle_dir
 
 		debug "Found: $f\n";
 
-		handle_rrd($rrd, $f, $multiplier);
+		handle_rrd($matches, $f);
 
 		$handled++;
 	}
@@ -179,22 +232,47 @@ if($help || !$ARGV[0]) {
 }
 
 foreach my $a (@ARGV) {
-	my ($rrd, $tun, $val) = split(/:/, $a);
-	if(!defined($val)) {
-		usage();
-		die "Exiting...";
+	my ($rrd, $tun, $val, $flags) = split(/:/, $a);
+	if(!defined($val) || $val eq "") {
+		die "Couldn't understand argument '$a'";
 	}
 	if(!$tune2info{$tun}) {
 		die "Don't understand $tun";
 	}
-	if(defined($tunings{$rrd}{$tun})) {
+	my $tref = {value => $val};
+	my $name = $rrd;
+	if($flags) {
+		foreach my $f (split(/,/, $flags)) {
+			if(!$flaghelp{$f}) {
+				die "Unknown flag: $f";
+			}
+			$tref->{flags}{$f} = 1;
+
+			if($f eq 'regex') {
+				# Verify that rrd name is a valid RE
+				eval {
+					"s" =~ m !$rrd!;
+				};
+				if($@) {
+					die "$rrd not a valid regex:\n",$@,"\n";
+				}
+
+				$name = "-$rrd-"; # Unique placeholder
+
+ 				$relist{$rrd}{$tun} = $tref;
+			}
+		}
+	}
+	if($tunings{$name} && $tunings{$name}{$tun}) {
 		die "Can't specify $tun twice for $rrd";
 	}
-
-	$tunings{$rrd}{$tun} = $val;
+		
+	$tunings{$name}{$tun} = $tref;
 }
 
 debug "tunings:\n", Dumper \%tunings;
+
+debug "relist:\n", Dumper \%relist;
 
 chdir $rrddir || die "Unable to chdir to $rrddir: $!";
 
@@ -210,7 +288,7 @@ if($ENV{'RRDCACHED_ADDRESS'}) {
 }
 
 opendir(my $dh, ".") || die "opendir .: $!";
-my @dirs = grep { $_ !~ '^(\.|__SummaryInfo__)' && -d $_ } readdir($dh);
+my @dirs = sort grep { $_ !~ '^(\.|__SummaryInfo__)' && -d $_ } readdir($dh);
 closedir($dh);
 
 my $totaldirs = 0;
@@ -224,7 +302,7 @@ foreach my $dir (@dirs) {
 
 	# For top level, we need to ascend into the subdirectories
 	opendir(my $sdh, $dir) || die "opendir $dir: $!";
-	my @sdirs = grep { $_ !~ '^(\.|__SummaryInfo__)' && -d "$dir/$_" } readdir($sdh);
+	my @sdirs = sort grep { $_ !~ '^(\.|__SummaryInfo__)' && -d "$dir/$_" } readdir($sdh);
 	closedir($sdh);
 
 	my $subdirs = 0;
@@ -236,25 +314,20 @@ foreach my $dir (@dirs) {
 
 	debug "$dir: $subdirs of total " . scalar(@dirs) . " dirs considered\n";
 
-	# Also add limits to SummaryInfo rrd:s, use number of subdirs as
-	# multiplier.
-	handle_dir("$dir/__SummaryInfo__", $subdirs);
+	# Also add limits to SummaryInfo rrd:s if they exist, might not if
+	# we are one level down in the tree.
+	if(-d "$dir/__SummaryInfo__") {
+		handle_dir("$dir/__SummaryInfo__");
+		# FIXME: Set flag so we know how to handle top-level SummaryInfo
+	}
 
 	$totaldirs += $subdirs;
 	$totalsubdirs += $subdirs;
 }
 
-debug "totaldirs: $totaldirs\n";
+debug "totaldirs=$totaldirs totalsubdirs=$totalsubdirs\n";
 
 # Also add limits to top level SummaryInfo rrd:s
-if($totaldirs) {
-	my $multiplier = $totaldirs;
-	if($totalsubdirs) {
-		# Avoid setting an insanely large limit on the top-level
-		# SummaryInfo, sqrt() chosen on a whim.
-		$multiplier = int(sqrt($totaldirs));
-	}
-	handle_dir("__SummaryInfo__", $multiplier);
-}
+handle_dir("__SummaryInfo__");
 
 exit 0;
